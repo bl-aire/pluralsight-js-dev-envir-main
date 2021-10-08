b10ae2ccc19fb179abfbee8df602f8082f0c26f9
Blaire <ogoh_blessing@yahoo.com>
2021-10-08 09:41:30 -0700

    Rounded up with Automation.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -9,10 +9,14 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
-app.listen(port, function(err){
+app.listen(3000, ()=>{
+  require('child_process').exec('start http://localhost:3000/');
+});
+
+/*app.listen(port, function(err){
   if (err) {
     console.log(err);
   } else {
-    open('http://localhost:' + port);
+    open('http://localhost:' + port);..this wasn't opening the page automatically on my browser so i used the node in-built child process
   }
-});
+});*/
