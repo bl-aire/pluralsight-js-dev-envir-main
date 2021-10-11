70a4e4cb5763fb1d8e4c0e01162e689f8a7b07d5
Blaire <ogoh_blessing@yahoo.com>
2021-10-11 06:14:52 -0700

    ESLint-watch set up

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,14 +18,14 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
-app.listen(3000, ()=>{
-  require('child_process').exec('start http://localhost:3000/');
-});
+/*app.listen(3000, ()=>{
+  require('child_process').exec('start http://localhost:3000/'); this opens localtunnel automatically but results to errors as open and path arent used
+});*/
 
-/*app.listen(port, function(err){
+app.listen(port, function(err){
   if (err) {
     console.log(err);
   } else {
-    open('http://localhost:' + port);..this wasn't opening the page automatically on my browser so i used the node in-built child process
+    open('http://localhost:' + port);
   }
-});*/
+});
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format( '$0,0.00'); //display 1000 in this format
-console.log(`I would pay ${courseValue} for this awesome course!`); //eslint-disable-line no-console 
+console.log(`I would pay ${courseValue} for this awesome course!`); //eslint-disable-line no-console
