b0d56bbdf9c7f7c953fc7eb2987fc2893fa45e35
Blaire <ogoh_blessing@yahoo.com>
2021-10-16 12:19:40 -0700

    Http calls set up

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import path from 'path';
-import open from 'open';
+//import open from 'open';......not used by pete
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
 /* eslint-disable no-console */
 
-const port = 3000;
+//const port = 3000;......not used by pete
 const app = express();
 const compiler = webpack(config);
 
@@ -18,14 +18,23 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
-/*app.listen(3000, ()=>{
-  require('child_process').exec('start http://localhost:3000/'); this opens localtunnel automatically but results to errors as open and path arent used
-});*/
+app.get('/users', function(req, res) {
+  //Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1, "firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2, "firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3, "firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+  ]);
+});
 
-app.listen(port, function(err){
+app.listen(3000, ()=>{
+  require('child_process').exec('start http://localhost:3000/'); //this opens localtunnel automatically but results to errors as open and path arent used
+});
+
+/*app.listen(port, function(err){
   if (err) {
     console.log(err);
   } else {
     open('http://localhost:' + port);
   }
-});
+});*/
